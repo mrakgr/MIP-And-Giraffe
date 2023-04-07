af8e477a04b0585a3f64e633dd07493ac3b54f37
Marko Grdinić <mrakgr@gmail.com>
2023-04-07 18:46:56 +0200

    Let's stop here for today.

--- MIP And Giraffe/Program.fs
+++ MIP And Giraffe/Program.fs
@@ -1,13 +1,43 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Giraffe
+
+module Pages =
+    open Giraffe.ViewEngine
+    
+    let master title_str body_list =
+        html [] [
+            head [] [
+                title [] [
+                    str title_str
+                ]
+            ]
+            body [] body_list
+        ]
+    
+    let index = master "Index" [
+        str "Hello World!"
+    ]
+        
+    let error = master "Error" [
+        str "404 - Page Not Found"
+    ]
+        
+let webApp : HttpHandler =
+    choose [
+        route "/" >=> htmlView Pages.index
+        setStatusCode 404 >=> htmlView Pages.error
+    ]
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddGiraffe() |> ignore
+            
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.UseGiraffe(webApp)
 
     app.Run()
 
